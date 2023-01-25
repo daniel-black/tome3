7c189e7ddc0d34861323f7477bdb49ffa15adf5b
daniel-black <drblack651@gmail.com>
2023-01-24 18:35:56 -0700

    basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <div>
-      Landing page
+    <div className="py-16 text-center">
+      <h1 className="font-bold text-4xl">Tome.</h1>
+      <p className="font-semibold text-2xl">Your library, online</p>
+      <div className="mt-8 space-x-5 text-blue-300">
+        <Link
+          href={'/'}
+          className='underline hover:text-blue-400'
+        >
+          Login
+        </Link>
+        <span className="select-none">·</span>
+        <Link
+          href={'/'}
+          className='underline hover:text-blue-400'
+        >
+          Register
+        </Link>
+      </div>
     </div>
   );
 }
